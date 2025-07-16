7e21870ec56cf46aa5502918e7ddc8157d10f5ee
luisgoweb <luismga2012@gmail.com>
2025-07-16 13:47:30 -0400

    store.ts: se crea función para añadir id al paciente con uuid

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,9 +1,18 @@
-
+import { usePatientStore } from "../store"
 
 const PatientsList = () => {
+
+    const patients = usePatientStore(state => state.patients)
+
   return (
     <div>
-      Listado
+      {patients.map( patients => (
+        <div key={patients.id}>
+            <p>{patients.name}</p>
+            <p>{patients.caretaker}</p>
+            <p>{patients.email}</p>
+        </div>
+      ))}
     </div>
   )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,22 @@
+import { v4 } from "uuid";
 import { create } from "zustand";
-import type { DraftPatient, Patient } from "./src/types";
+import type { DraftPatient, Patient } from "./types";
 
 type PatientState = {
     patients: Patient[]
     addPatient: (data: DraftPatient) => void
 }
 
-export const usePatientStore = create<PatientState>(()=> ({
+const createPatient = (patient: DraftPatient) : Patient => {
+    return {...patient, id: v4()}
+}
+
+export const usePatientStore = create<PatientState>((set)=> ({
     patients: [],
     addPatient: (data) => {
-        console.log(data)
+        const newPatient = createPatient(data)
+        set((state) => ({
+            patients: [...state.patients, newPatient]
+        }))
     }
 }))
