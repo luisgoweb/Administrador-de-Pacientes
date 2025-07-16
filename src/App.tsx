ffcadce0c0c464ea98c51342c8ec24710dd693cd
luisgoweb <luismga2012@gmail.com>
2025-07-16 09:51:48 -0400

    App.tsx: Creado el header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ function App() {
 
   return (
     <>
-      <header className="text-4xl text-center font-bold">Administrador de pacientes</header>
+      <div className="container mx-auto mt-10">
+          <h1 className="text-5xl font-black text-center md:w-2/3 md:mx-auto">
+            Seguimiento de Pacientes{' '}
+            <span className="text-indigo-700">Veterinaria</span>
+          </h1>
+      </div>
+
+      <div className="mt-12 md:flex">
+
+      </div>
+
     </>
   )
 }
